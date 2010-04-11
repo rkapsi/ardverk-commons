1c32723bb88ea7cd0baeb78cd269f98ff81a74d7
Roger Kapsi <rkapsi@gmail.com>
2010-04-11 18:56:43 -0400

    Adding a few more classes

---
+++ src/main/java/org/ardverk/collection/FixedSizeArrayList.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2010 Roger Kapsi
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package org.ardverk.collection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class FixedSizeArrayList<E> extends ArrayList<E> implements FixedSize {
+
+    private static final long serialVersionUID = -8835179064931233224L;
+    
+    private final int maxSize;
+
+    public FixedSizeArrayList(int maxSize) {
+        super();
+        
+        if (maxSize < 0) {
+            throw new IllegalArgumentException("maxSize=" + maxSize);
+        }
+        
+        this.maxSize = maxSize;
+    }
+
+    public FixedSizeArrayList(Collection<? extends E> c, int maxSize) {
+        super(c);
+        
+        if (maxSize < 0) {
+            throw new IllegalArgumentException("maxSize=" + maxSize);
+        }
+        
+        this.maxSize = maxSize;
+    }
+
+    public FixedSizeArrayList(int initialCapacity, int maxSize) {
+        super(initialCapacity);
+        
+        if (maxSize < 0) {
+            throw new IllegalArgumentException("maxSize=" + maxSize);
+        }
+        
+        this.maxSize = maxSize;
+    }
+    
+    @Override
+    public int getMaxSize() {
+        return maxSize;
+    }
+    
+    @Override
+    public boolean isFull() {
+        return size() >= getMaxSize();
+    }
+
+    @Override
+    public boolean add(E e) {
+        adjustSize(size(), e);
+        return super.add(e);
+    }
+
+    @Override
+    public void add(int index, E element) {
+        if (index < 0 || size() < index) {
+            throw new IllegalArgumentException("index=" + index);
+        }
+        
+        adjustSize(index, element);
+        super.add(index, element);
+    }
+    
+    private void adjustSize(int index, E e) {
+        if (isFull()) {
+            E old = remove(eject(index, e));
+            removed(old);
+        }
+    }
+    
+    protected int eject(int index, E e) {
+        return 0;
+    }
+    
+    protected void removed(E element) {
+        // OVERRIDE
+    }
+}
---
+++ src/main/java/org/ardverk/collection/FixedSizeHashMap.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2010 Roger Kapsi
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package org.ardverk.collection;
+
+import java.io.Serializable;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * A {@link Map} with a fixed-size capacity.
+ */
+public class FixedSizeHashMap<K, V> extends LinkedHashMap<K, V> 
+        implements FixedSize, Serializable {
+    
+    private static final long serialVersionUID = -8289709441678695668L;
+    
+    protected final int maxSize;
+
+    public FixedSizeHashMap(int maxSize) {
+        super();
+        
+        if (maxSize < 0) {
+            throw new IllegalArgumentException("maxSize=" + maxSize);
+        }
+        
+        this.maxSize = maxSize;
+    }
+
+    public FixedSizeHashMap(int initialCapacity, float loadFactor, 
+            boolean accessOrder, int maxSize) {
+        super(initialCapacity, loadFactor, accessOrder);
+        
+        if (maxSize < 0) {
+            throw new IllegalArgumentException("maxSize=" + maxSize);
+        }
+        
+        this.maxSize = maxSize;
+    }
+
+    public FixedSizeHashMap(int initialCapacity, float loadFactor, int maxSize) {
+        super(initialCapacity, loadFactor);
+        
+        if (maxSize < 0) {
+            throw new IllegalArgumentException("maxSize=" + maxSize);
+        }
+        
+        this.maxSize = maxSize;
+    }
+
+    public FixedSizeHashMap(int initialCapacity, int maxSize) {
+        super(initialCapacity);
+        
+        if (maxSize < 0) {
+            throw new IllegalArgumentException("maxSize=" + maxSize);
+        }
+        
+        this.maxSize = maxSize;
+    }
+
+    public FixedSizeHashMap(Map<? extends K, ? extends V> m, int maxSize) {
+        super(m);
+        
+        if (maxSize < 0) {
+            throw new IllegalArgumentException("maxSize=" + maxSize);
+        }
+        
+        this.maxSize = maxSize;
+    }
+
+    @Override
+    public int getMaxSize() {
+        return maxSize;
+    }
+    
+    @Override
+    public boolean isFull() {
+        return size() >= getMaxSize();
+    }
+    
+    protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+        if (size() > getMaxSize()) {
+            removing(eldest);
+            return true;
+        }
+        return false;
+    }
+    
+    /**
+     * 
+     */
+    protected void removing(Map.Entry<K, V> eldest) {
+        // OVERRIDE
+    }
+}
