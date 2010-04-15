97e4cbdc0c59155d669358a71b5046f80327af33
Roger Kapsi <rkapsi@gmail.com>
2010-04-15 08:55:42 -0400

    Adding a copy method

--- src/main/java/org/ardverk/collection/CollectionsUtils.java
+++ src/main/java/org/ardverk/collection/CollectionsUtils.java
@@ -25,16 +25,23 @@ public class CollectionsUtils {
     private CollectionsUtils() {}
     
     /**
+     * Makes a copy of src and returns it.
+     */
+    public static <K, V> Map<K, V> copy(Map<? extends K, ? extends V> src) {
+        return copyTo(src, Collections.<K, V>emptyMap());
+    }
+    
+    /**
      * Copies src to dst if src is not null and not empty. It 
      * will create and return a new Map if dst is either null 
      * or {@link Collections#EMPTY_MAP}.
      */
-    public static Map<Object, Object> copyTo(
-            Map<?, ?> src, Map<Object, Object> dst) {
+    public static <K, V> Map<K, V> copyTo(
+            Map<? extends K, ? extends V> src, Map<K, V> dst) {
         
         if (src != null && !src.isEmpty()) {
             if (dst == null || dst.equals(Collections.EMPTY_MAP)) {
-                dst = new HashMap<Object, Object>(src);
+                dst = new HashMap<K, V>(src);
             } else {
                 dst.putAll(src);
             }
