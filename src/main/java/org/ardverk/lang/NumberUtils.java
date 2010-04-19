da27169e421a7f2734347c9a3d0d111120b1378c
Roger Kapsi <rkapsi@gmail.com>
2010-04-19 08:39:19 -0400

    Adding a bunch of getBoolean(), getInteger() and so forth methods...

--- src/main/java/org/ardverk/lang/NumberUtils.java
+++ src/main/java/org/ardverk/lang/NumberUtils.java
@@ -16,6 +16,9 @@
 
 package org.ardverk.lang;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
+
 public class NumberUtils {
 
     private NumberUtils() {}
@@ -36,5 +39,131 @@ public class NumberUtils {
      */
     public static boolean isUndefined(float value) {
         return Float.isNaN(value) || Float.isInfinite(value);
+    }
+    
+    /**
+     * 
+     */
+    public static boolean getBoolean(Object value) {
+        return getBoolean(value, false, false);
+    }
+    
+    /**
+     * 
+     */
+    public static boolean getBoolean(Object value, boolean defaultValue) {
+        return getBoolean(value, defaultValue, true);
+    }
+    
+    /**
+     * 
+     */
+    private static boolean getBoolean(Object value, 
+            boolean defaultValue, boolean hasDefault) {
+        
+        if (value instanceof Boolean) {
+            return ((Boolean)value).booleanValue();
+        } else if (value instanceof AtomicBoolean) {
+            return ((AtomicBoolean)value).get();
+        }
+        
+        if (hasDefault) {
+            return defaultValue;
+        }
+        
+        throw new IllegalArgumentException("value=" + value);
+    }
+    
+    /**
+     * 
+     */
+    public static int getInteger(Object value) {
+        return getInteger(value, -1, false);
+    }
+    
+    /**
+     * 
+     */
+    public static int getInteger(Object value, int defaultValue) {
+        return getInteger(value, defaultValue, true);
+    }
+    
+    /**
+     * 
+     */
+    private static int getInteger(Object value, 
+            int defaultValue, boolean hasDefault) {
+        
+        if (value instanceof Number) {
+            return ((Number)value).intValue();
+        }
+        
+        if (hasDefault) {
+            return defaultValue;
+        }
+        
+        throw new IllegalArgumentException("value=" + value);
+    }
+    
+    /**
+     * 
+     */
+    public static float getFloat(Object value) {
+        return getFloat(value, Float.NaN, false);
+    }
+    
+    /**
+     * 
+     */
+    public static float getFloat(Object value, float defaultValue) {
+        return getFloat(value, defaultValue, true);
+    }
+    
+    /**
+     * 
+     */
+    private static float getFloat(Object value, 
+            float defaultValue, boolean hasDefault) {
+        
+        if (value instanceof Number) {
+            return ((Number)value).floatValue();
+        }
+        
+        if (hasDefault) {
+            return defaultValue;
+        }
+        
+        throw new IllegalArgumentException("value=" + value);
+    }
+    
+    /**
+     * 
+     */
+    public static double getDouble(Object value) {
+        return getDouble(value, Double.NaN, false);
+    }
+    
+    /**
+     * 
+     */
+    public static double getDouble(Object value, double defaultValue) {
+        return getDouble(value, defaultValue, true);
+    }
+    
+    /**
+     * 
+     */
+    private static double getDouble(Object value, 
+            double defaultValue, boolean hasDefault) {
+        
+        if (value instanceof Number) {
+            return ((Number)value).doubleValue();
+        }
+        
+        if (hasDefault) {
+            return defaultValue;
+        }
+        
+        throw new IllegalArgumentException("value=" + value);
     }
 }
