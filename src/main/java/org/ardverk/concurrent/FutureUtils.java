c11cff19c4dbf4005ed5efd146006f94fddc6eba
Roger Kapsi <rkapsi@gmail.com>
2010-10-21 09:28:41 -0400

    Adding license and JavaDoc

--- src/main/java/org/ardverk/concurrent/FutureUtils.java
+++ src/main/java/org/ardverk/concurrent/FutureUtils.java
@@ -1,8 +1,27 @@
+/*
+ * Copyright 2010 Roger Kapsi
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
 package org.ardverk.concurrent;
 
 import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicReference;
 
+/**
+ * Misc utilities for {@link Future}s.
+ */
 public class FutureUtils {
 
     private FutureUtils() {}
@@ -19,7 +38,7 @@ public class FutureUtils {
     }
     
     /**
-     * Cancels the given {@link AtomicReference} of an {@link Future}.
+     * Cancels the given {@link AtomicReference} of a {@link Future}.
      */
     public static boolean cancel(AtomicReference<? extends Future<?>> futureRef, 
             boolean mayInterruptIfRunning) {
