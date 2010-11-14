7a4c65e3076e43abd9258a0e65f3b3ad436f7287
Roger Kapsi <rkapsi@gmail.com>
2010-11-14 13:16:08 -0500

    Adding a Comparator for long values

---
+++ src/main/java/org/ardverk/utils/LongComparator.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2010 Roger Kapsi
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package org.ardverk.utils;
+
+import java.io.Serializable;
+import java.util.Comparator;
+
+/**
+ * A {@link Comparator} for long values.
+ */
+public class LongComparator implements Comparator<Long>, Serializable {
+    
+    private static final long serialVersionUID = -611530289701062817L;
+
+    public static final LongComparator COMPARATOR = new LongComparator();
+    
+    @Override
+    public int compare(Long o1, Long o2) {
+        return compare(o1.longValue(), o2.longValue());
+    }
+    
+    /**
+     * Compares the given long values.
+     */
+    public static int compare(long l1, long l2) {
+        if (l1 < l2) {
+            return -1;
+        } else if (l2 < l1) {
+            return 1;
+        }
+        return 0;
+    }
+}
