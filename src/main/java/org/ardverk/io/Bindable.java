6e46b4059ede8a104bf09425102f572ae4f254b6
Roger Kapsi <rkapsi@gmail.com>
2010-12-12 16:00:21 -0500

    Adding a new Bindable interface

---
+++ src/main/java/org/ardverk/io/Bindable.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2010 Roger Kapsi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ardverk.io;
+
+import java.io.IOException;
+
+/**
+ * An I/O objects that is bindable to some other object.
+ */
+public interface Bindable<T> {
+
+    /**
+     * Binds this to the given object.
+     */
+    public void bind(T t) throws IOException;
+    
+    /**
+     * Unbinds this from the other object.
+     */
+    public void unbind() throws IOException;
+    
+    /**
+     * Returns {@code true} if this object is bound to some other Object.
+     */
+    public boolean isBound();
+}
