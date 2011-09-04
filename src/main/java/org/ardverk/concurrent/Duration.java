8decb1e3a7a3c15fc9a89ce0d6938fdf27c32a49
Roger Kapsi <rkapsi@gmail.com>
2011-09-04 09:00:20 -0400

    Adding a class that can hold a time and its unit

---
+++ src/main/java/org/ardverk/concurrent/Duration.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2009-2011 Roger Kapsi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ardverk.concurrent;
+
+import java.io.Serializable;
+import java.util.concurrent.TimeUnit;
+
+import org.ardverk.lang.Longs;
+
+public class Duration implements Comparable<Duration>, Serializable {
+    
+    private static final long serialVersionUID = -7966576555871244178L;
+
+    private final long duration;
+    
+    private final TimeUnit unit;
+    
+    public Duration(long duration, TimeUnit unit) {
+        this.duration = duration;
+        this.unit = unit;
+    }
+    
+    public long getTime(TimeUnit unit) {
+        return unit.convert(duration, this.unit);
+    }
+    
+    public long getTimeInMillis() {
+        return getTime(TimeUnit.MILLISECONDS);
+    }
+    
+    @Override
+    public int compareTo(Duration o) {
+        return Longs.compare(getTimeInMillis(), o.getTimeInMillis());
+    }
+
+    @Override
+    public int hashCode() {
+        return (int)getTimeInMillis();
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        } else if (!(o instanceof Duration)) {
+            return false;
+        }
+        
+        return getTimeInMillis() == ((Duration)o).getTimeInMillis();
+    }
+    
+    @Override
+    public String toString() {
+        return duration + " " + unit;
+    }
+}
