2e6492fa73148e063ea3d189bc52d2a8a3469582
Roger Kapsi <rkapsi@gmail.com>
2011-10-30 12:24:08 -0400

    JavaDoc

--- src/main/java/org/ardverk/enums/ObjectEnum.java
+++ src/main/java/org/ardverk/enums/ObjectEnum.java
@@ -1,12 +1,13 @@
 package org.ardverk.enums;
 
 /**
- * 
+ * An interface for {@link Enum}s that provide an alternative {@link Object}
+ * based value of themselves.
  */
 public interface ObjectEnum<T> {
 
     /**
-     * 
+     * Returns the {@link Enum}'s alternative value.
      */
     public T convert();
 }
--- src/main/java/org/ardverk/enums/PrimitiveEnum.java
+++ src/main/java/org/ardverk/enums/PrimitiveEnum.java
@@ -1,11 +1,21 @@
 package org.ardverk.enums;
 
+/**
+ * An interface for {@link Enum}'s that provide an alternative value
+ * of themselves.
+ * 
+ * @see PrimitiveEnum.Int
+ */
 public interface PrimitiveEnum {
     
     /**
      * An {@link PrimitiveEnum} that is backed by {@code int} values.
      */
     public static interface Int extends PrimitiveEnum {
+        
+        /**
+         * Returns the {@link Enum}'s alternative {@code int} value.
+         */
         public int convert();
     }
 }
