3bba8670d8bfdfa05be97a387c1dcaf361363a0a
Roger Kapsi <rkapsi@gmail.com>
2011-10-30 12:09:38 -0400

    Moving it to an inner class

--- src/main/java/org/ardverk/enums/PrimitiveEnums.java
+++ src/main/java/org/ardverk/enums/PrimitiveEnums.java
@@ -4,64 +4,70 @@ import java.lang.reflect.Array;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class PrimitiveEnums {
-
-    private static final ConcurrentHashMap<Class<?>, Enum<?>[]> INT_TABLE 
-        = new ConcurrentHashMap<Class<?>, Enum<?>[]>();
     
-    /**
-     * 
-     */
-    @SuppressWarnings("unchecked")
-    public static <V extends Enum<V> & PrimitiveEnum.Int> V[] of(Class<V> type) {
-        V[] values = (V[])INT_TABLE.get(type);
-        if (values == null) {
-            values = createTable(type);
-            Enum<?>[] existing = INT_TABLE.putIfAbsent(type, values);
-            if (existing != null) {
-                values = (V[])existing;
+    public static class Int {
+        
+        private static final ConcurrentHashMap<Class<?>, Enum<?>[]> TABLE 
+            = new ConcurrentHashMap<Class<?>, Enum<?>[]>();
+        
+        /**
+         * 
+         */
+        @SuppressWarnings("unchecked")
+        public static <V extends Enum<V> & PrimitiveEnum.Int> V[] of(Class<V> type) {
+            V[] values = (V[])TABLE.get(type);
+            if (values == null) {
+                values = createTable(type);
+                Enum<?>[] existing = TABLE.putIfAbsent(type, values);
+                if (existing != null) {
+                    values = (V[])existing;
+                }
             }
-        }
-        return values;
-    }
-    
-    /**
-     * 
-     */
-    public static <V extends Enum<V> & PrimitiveEnum.Int> V valueOf(Class<V> type, int key) {
-        V[] values = of(type);
-        V value = values[(key & Integer.MAX_VALUE) % values.length];
-        
-        if (value != null && value.convert() == key) {
-            return value;
+            return values;
         }
         
-        throw new IllegalArgumentException("key=" + key);
-    }
-    
-    /**
-     * 
-     */
-    @SuppressWarnings("unchecked")
-    private static <V extends Enum<V> & PrimitiveEnum.Int> V[] createTable(Class<V> type) {
-
-        V[] values = type.getEnumConstants();
-        
-        Class<?> componentType = values.getClass().getComponentType();
-        V[] dst = (V[])Array.newInstance(componentType, values.length);
-
-        for (V value : values) {
-            int index = (value.convert() & Integer.MAX_VALUE) % values.length;
+        /**
+         * 
+         */
+        public static <V extends Enum<V> & PrimitiveEnum.Int> V valueOf(Class<V> type, int key) {
+            V[] values = of(type);
+            V value = values[(key & Integer.MAX_VALUE) % values.length];
             
-            V existing = dst[index];
-            if (existing != null) {
-                throw new IllegalStateException("Conflict: " + value + " vs. " + existing);
+            if (value != null && value.convert() == key) {
+                return value;
             }
             
-            dst[index] = value;
+            throw new IllegalArgumentException("key=" + key);
         }
-
-        return dst;
+        
+        /**
+         * 
+         */
+        @SuppressWarnings("unchecked")
+        private static <V extends Enum<V> & PrimitiveEnum.Int> V[] createTable(Class<V> type) {
+    
+            V[] values = type.getEnumConstants();
+            
+            Class<?> componentType = values.getClass().getComponentType();
+            V[] dst = (V[])Array.newInstance(componentType, values.length);
+    
+            for (V value : values) {
+                int index = (value.convert() & Integer.MAX_VALUE) % values.length;
+                
+                V existing = dst[index];
+                if (existing != null) {
+                    throw new IllegalStateException("Conflict: " + value + " vs. " + existing);
+                }
+                
+                dst[index] = value;
+            }
+    
+            return dst;
+        }
+    
+        private Int() {}
     }
+    
     
     private PrimitiveEnums() {}
 }
